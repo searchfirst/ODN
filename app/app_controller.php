b8cbe7f8f8ce99f0d2c1ecbe1cd04b0304b22d26
Paul Connolley <shunuk@gmail.com>
2011-09-11 20:43:30 +0100

    Changed security settings in core

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,16 +1,16 @@
 <?php 
 class AppController extends Controller {
-	var $components = array(
-		'Acl', 'Auth', 'Dux', 'RequestHandler',
-		'Session', 'Sma.Sma'/*, 'AclMenu.AclMenu'*/
-	);
-	var $helpers = array(
-		'Status', 'Html', 'Form', 'Time', 'TextAssistant', 'Js', 'Session', 'Sma.Sma'
-	);
-	var $uses = array('User');
-	var $view = 'Theme';
-	
-	function beforeRender() {
-		if($external_links = Configure::read('Dux.external_links')) $this->set('external_links',$external_links);
-	}
+    var $components = array(
+        'Acl', 'Auth', 'Dux', 'RequestHandler',
+        'Session', 'Sma.Sma'/*, 'AclMenu.AclMenu'*/
+    );
+    var $helpers = array(
+        'Status', 'Html', 'Form', 'Time', 'TextAssistant', 'Js', 'Session', 'Sma.Sma'
+    );
+    var $uses = array('User');
+    var $view = 'Theme';
+    
+    function beforeRender() {
+        if($external_links = Configure::read('Dux.external_links')) $this->set('external_links',$external_links);
+    }
 }
--- app/config/core.php
+++ app/config/core.php
@@ -4,13 +4,13 @@ Configure::write('App.encoding', 'UTF-8');
 define('LOG_ERROR', 2);
 Configure::write('Session.save', 'cake');
 Configure::write('Session.cookie', 'Dux');
-Configure::write('Session.timeout', '300000000');
+Configure::write('Session.timeout', '2592');
 Configure::write('Session.start', true);
 Configure::write('Session.checkAgent', true);
 Configure::write('Security.level', 'low');
 Configure::write('Security.salt', 'DYhG93b0qyJfI9981CNCoUubWwvniR2G0FgaC9mi');
 Configure::write('Acl.classname', 'DbAcl');
 Configure::write('Acl.database', 'default');
-Configure::write('Cache.check', true);
+Configure::write('Cache.check', false);
 Cache::config('default', array('engine' => 'File'));
 ?>
