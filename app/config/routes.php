2774169372ce5b3e780d4fce2aafb00482853d6b
Paul Connolley <shunuk@gmail.com>
2011-09-15 12:05:10 +0100

    Removed unnecessary routes from routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,5 +3,3 @@ Router::mapResources(array( 'users','customers','contacts','notes','services','w
 Router::parseExtensions();
 Router::connect('/',array('controller'=>'facades','action'=>'index'));
 Router::connect('/customers/filter/:page',array('controller'=>'customers','action'=>'index'),array('page'=>'[a-z1-9]{1}'));
-Router::connect('/rss/:controller/:action/*',array('alt_content'=>'Rss','url'=>array('ext'=>'rss')));
-Router::connect('/pdf/:controller/:action/*',array('alt_content'=>'Pdf','url'=>array('ext'=>'pdf')));
