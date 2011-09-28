d5f0f7cf3c630d04dbced502122f122c134b6d21
Paul Connolley <shunuk@gmail.com>
2011-09-28 13:47:02 +0100

    Bugfix on RemoveEmptyRelationshipsBehavior class

--- app/models/behaviors/remove_empty_relationships.php
+++ app/models/behaviors/remove_empty_relationships.php
@@ -1,14 +1,16 @@
 <?php
 class RemoveEmptyRelationshipsBehavior extends ModelBehavior {
-	function afterFind($model, $results, $primary) {
-		if ($primary && !empty($results[0])) {
-			$assocKeys = array_merge(array_keys($model->belongsTo),array_keys($model->hasOne));
-			foreach ($assocKeys as $assoc) {
-				if (!empty($results[0][$assoc]) && empty($results[0][$assoc]['id'])) {
-					$results[0][$assoc] = null;
-				}
-			}
-		}
-		return $results;
-	}
+    function afterFind($model, $results, $primary) {
+        if ($primary && is_array($results)) {
+            $assocKeys = array_merge(array_keys($model->belongsTo),array_keys($model->hasOne));
+            foreach ($results as $r => $result) {
+                foreach ($assocKeys as $assoc) {
+                    if (array_key_exists($assoc, $result) && empty($result[$assoc]['id'])) {
+                        $results[$r][$assoc] = null;
+                    }
+                }
+            }
+        }
+        return $results;
+    }
 }
