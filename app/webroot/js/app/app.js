a8b1a3f3ce53acc00c318335570aca1b74fb7106
Paul Connolley <paul.connolley@gmail.com>
2011-11-14 22:39:55 +0000

    Renamed namespace to ODN

--- app/webroot/js/app/app.js
+++ app/webroot/js/app/app.js
@@ -1,20 +1,24 @@
-    $(function(){
-        var duxApp = {
-            appRouter: new dac.AppRouter,
-            facadesRouter: new dac.FacadesRouter,
-            customersRouter: new dac.CustomersRouter,
-            invoicesRouter: new dac.InvoicesRouter,
-            utilitiesRouter: new dac.UtilitiesRouter,
-            searchView: new dac.SearchView({viewTemplate: 'searchView'})
-        };
-        $('nav#menu a').click(function(e){
-            var usedModifier = e.which == 1 && (e.altKey || e.ctrlKey || e.shiftKey || e.metaKey),
-                url = $(this).attr('href').match(/^\/?(.*)/)[1];
-            if (!usedModifier) {
-                e.preventDefault();
-                duxApp.appRouter.navigate(url,true);
-            }
-        });
-        Backbone.history.start({pushState:true});
-        window.duxApp = duxApp;
-    })
+$(function(){
+    var ODN = {
+        routers: {
+            app: new dac.AppRouter,
+            facades: new dac.FacadesRouter,
+            customers: new dac.CustomersRouter,
+            invoices: new dac.InvoicesRouter,
+            utilities: new dac.UtilitiesRouter
+        },
+        views: {
+            search: new dac.SearchView({viewTemplate: 'searchView'})
+        }
+    };
+    $('nav#menu a').click(function(e){
+        var usedModifier = e.which == 1 && (e.altKey || e.ctrlKey || e.shiftKey || e.metaKey),
+            url = $(this).attr('href').match(/^\/?(.*)/)[1];
+        if (!usedModifier) {
+            e.preventDefault();
+            ODN.routers.app.navigate(url, true);
+        }
+    });
+    Backbone.history.start({pushState:true});
+    window.ODN = ODN;
+})
